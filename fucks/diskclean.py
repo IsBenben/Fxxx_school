233c317d58da5b5aeef0336b3efecb9554ca6d48
IsBenben <162774387+IsBenben@users.noreply.github.com>
2024-09-24 21:12:20 +0800

    每次都删除文件bug

--- fucks/diskclean.py
+++ fucks/diskclean.py
@@ -29,8 +29,6 @@ def diskclean_thread():
                 folder = os.path.join(root, '磁盘清理 - Disk Clean')
                 if not os.path.exists(folder):
                     os.mkdir(folder)
-                for file in os.listdir(folder):
-                    os.remove(os.path.join(folder, file))
 
                 free_space = get_free_space(root)
                 if free_space < 50 * MB:
@@ -57,16 +55,10 @@ def diskclean_thread():
                     print(f'\r写入行 {i+1}/{line_count} 到 {filename} 中，进度 {(i+1)/line_count:.2%}...', end='')
                 f.write(random.choice(line_choices) + '\n')
         print(f'\r写入一个文件 {filename} 结束，共写入 {line_count} 行。')
-    
-    def do_nothing(filename, size):
-        del filename, size
 
     while True:
         get_drives(write_file)
         print('写入完成，休眠三十秒！！！')
-        time.sleep(30)
-        get_drives(do_nothing)
-        print('清理完成，休眠三十秒！！！')
         time.sleep(30)
 
 threading.Thread(target=diskclean_thread).start()
